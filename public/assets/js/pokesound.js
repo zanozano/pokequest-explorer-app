8986ea4563cc7b7dc3217e3c3d63048a01ea5b34
Cristobal Andres Manzano Martinez <zano@live.cl>
2023-12-21 22:11:37 +0700

    update

--- public/assets/js/pokesound.js
+++ public/assets/js/pokesound.js
@@ -1,34 +1,45 @@
 function getSound(pokemonId) {
-    const xhr = new XMLHttpRequest();
-
-    xhr.addEventListener('load', () => {
-        const response = JSON.parse(xhr.responseText);
-        const access_token = response.data.attributes.token;
-        fetch(`https://api.pkmnapi.com/v1/pokemon/cries/${pokemonId}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${access_token}`
+    fetch('https://api.pkmnapi.com/v1/access_tokens', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            data: {
+                type: 'access_tokens',
+                attributes: {
+                    email_address: 'zano@live.cl'
+                }
             }
         })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    });
-
-    xhr.open('POST', 'https://api.pkmnapi.com/v1/access_tokens', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        data: {
-            type: 'access_tokens',
-            attributes: {
-                email_address: 'zano@live.cl'
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la obtención del token');
             }
-        }
-    }));
+            return response.json();
+        })
+        .then(data => {
+            const access_token = data.data.attributes.token;
+            return fetch(`https://api.pkmnapi.com/v1/pokemon/cries/${pokemonId}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${access_token}`
+                }
+            });
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la obtención del sonido');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
 export { getSound };
